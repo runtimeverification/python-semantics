b00cab933cea1a65c3d99d6558bb1ce46cce57fb
Dwight Guth <dwight.guth@gmail.com>
2012-03-09 10:41:45 -0600

    add test cases for lists

---
+++ programs/testlists.py
@@ -0,0 +1,21 @@
+assert len([5]) == 1
+assert len([5,6]) == 2
+
+assert [5,6][0] == 5
+assert [5,6][1] == 6
+assert [5,6][-1] == 6
+assert [5,6][-2] == 5
+
+try:
+  [5,6][2]
+  assert False
+except IndexError:
+  x = 5
+assert x == 5
+
+try:
+  [5,6][-3]
+  assert False
+except IndexError:
+  x = 6
+assert x == 6
