35f9256d2d59ed75ad45162e805e42eb9205b473
Dwight Guth <dwight.guth@gmail.com>
2012-03-09 15:46:58 -0600

    add list test cases

--- programs/testlists.py
+++ programs/testlists.py
@@ -24,3 +24,12 @@ assert [True] == [1]
 assert not [] == [1]
 assert not [1] == []
 assert not [5] == [6]
+
+x = iter([5,6])
+assert x.__next__() == 5
+assert x.__next__() == 6
+try:
+  assert x.__next__() and False
+except StopIteration:
+  y = 5
+assert y == 5
