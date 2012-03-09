820126916644197242f96f458ec96acf82145fcf
Dwight Guth <dwight.guth@gmail.com>
2012-03-09 16:06:20 -0600

    fix functions test cases

--- programs/testfunctions.py
+++ programs/testfunctions.py
@@ -40,7 +40,7 @@ except TypeError:
 assert x == 6
 
 def d(x, y, ** z):
-  assert x == 5 and y == 6 and z["w"] == 7 and len(z) == 1
+  assert x == 5 and y == 6 and z["w"] == 7 and len(z) == 1, (x, y, z)
   return 0
 
 assert d(y=6,x=5,w=7) == 0
@@ -67,7 +67,7 @@ x = True
 def f(y=[]):
   if x: assert y == []
   else: assert y == [5]
-  y.append(5)
+  list.__init__(y, [5])
 
 assert f() is None
 x = False
