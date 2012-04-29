148f5cd0d6da90c0e0b8c381fd3b3e295bccb4cb
Dwight Guth <dwight.guth@gmail.com>
2012-04-29 14:56:46 -0500

    fix strings for python 3.2

--- programs/teststrings.py
+++ programs/teststrings.py
@@ -1,9 +1,16 @@
 assert "5" is "5"
 assert "5" == "5"
 assert not ("5" is "6")
-assert "5" .__add__("6") is str.__add__("5", "6") is "6" .__radd__("5") is "5" + "6" is "56"
-assert "5" .__add__(6) is NotImplemented
+assert "5" .__add__("6") == str.__add__("5", "6") == "5" + "6" == "56"
+
+try:
+  "5" .__add__(6)
+  assert False
+except TypeError:
+  x = 5
+assert x == 5
+
 assert "5" .__class__ is str
 assert str .__class__ is type
-assert str.__name__ is "str"
-assert hash("hello world") == -2324238377118044897, hash("hello world")
+assert str.__name__ == "str"
+assert hash("hello world") == -1500746465, hash("hello world")
