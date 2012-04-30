457f4ba0b6f146d0925e72e9b5238cea3991233c
Dwight Guth <dwight.guth@gmail.com>
2012-04-30 13:30:12 -0500

    add test for subtle ref(id(O)) bug

--- programs/testlists5.py
+++ programs/testlists5.py
@@ -1,7 +1,7 @@
 assert list((5,)) == [5]
 assert list() == []
 assert [True] == [1]
-assert [1, 2, 3] == [1, 2, 3]
+assert [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] == list([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 assert not [] == [1]
 assert not [1] == []
 assert not [5] == [6]
