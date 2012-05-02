76632560265c2e733640dd39e9cd855a86264fa6
Dwight Guth <dwight.guth@gmail.com>
2012-05-01 20:50:38 -0500

    add more tests for docstrings

--- programs/testdocstring.py
+++ programs/testdocstring.py
@@ -16,3 +16,19 @@ assert b.__doc__ == "5"
 def c(): pass
 
 assert c.__doc__ is None
+
+def d():
+  b"5"
+
+assert d.__doc__ is None
+
+def e():
+  "5" + "6"
+
+assert e.__doc__ is None
+
+def f():
+  x = 5
+  y = 6
+
+assert f.__doc__ is None
