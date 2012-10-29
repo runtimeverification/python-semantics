5acd3185b3a6fdc6e236aa0d019f04753243b8e7
Dwight Guth <dwight.guth@gmail.com>
2012-10-29 14:05:30 -0500

    add mro to classes test

--- programs/testclasses1.py
+++ programs/testclasses1.py
@@ -5,12 +5,15 @@ assert A.__doc__ is None
 assert A.__module__ == "__main__"
 assert A.__name__ == "A"
 assert A.__bases__ == (object,)
+assert A.__mro__ == (A, object)
 assert A.x == 5
 
 class B(int, A): pass
 
 assert B.__bases__ == (int, A)
+assert B.__mro__ == (B, int, A, object)
 
-class C(A, * (int, B)): pass
+class C(B, * (int, A)): pass
 
-assert C.__bases__ == (A, int, B)
+assert C.__bases__ == (B, int, A)
+assert C.__mro__ == (C, B, int, A, object)
