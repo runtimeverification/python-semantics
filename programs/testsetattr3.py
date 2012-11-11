ac8cc279ed2c35c8b540096c9237035df71b326d
Dwight Guth <dwight.guth@gmail.com>
2012-11-10 20:08:46 -0600

    add an attr to setattr tests

--- programs/testsetattr3.py
+++ programs/testsetattr3.py
@@ -1,9 +1,22 @@
 def a(): pass
 
 x = 5
+y = "x"
 a.__module__ = x
+a.__name__ = y
 assert a.__module__ is x == 5
+assert a.__name__ == y == "x"
 
 x = 6
+y = "y"
 setattr(a, "__module__", x)
+setattr(a, "__name__", y)
 assert a.__module__ is x == 6
+assert a.__name__ == y == "y"
+
+try:
+  a.__name__ = 5
+  assert False
+except TypeError:
+  z = 1
+assert z == 1
