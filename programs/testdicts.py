740596df81bfa3510c14d02aa66bc378acced6ec
Dwight Guth <dwight.guth@gmail.com>
2012-11-11 19:10:35 -0600

    fix testdicts

--- programs/testdicts.py
+++ programs/testdicts.py
@@ -1,7 +1,7 @@
 x = {"5": "6", "6": "7"}
-assert x["5"] is "6" and x["6"] is "7"
+assert x["5"] == "6" and x["6"] == "7"
 x["7"] = "8"
-assert x["7"] is "8"
+assert x["7"] == "8"
 del x["5"]
 
 try:
