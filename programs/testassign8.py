dc4d2da4a85b53bf795e4a0bf29ee7a3d36a6594
Dwight Guth <dwight.guth@gmail.com>
2012-11-11 19:53:27 -0600

    add debug information to testassign8

--- programs/testassign8.py
+++ programs/testassign8.py
@@ -1,2 +1,2 @@
 a, b, * c, x, y, z = (1, 2, 3, 4, 5, 6, 7, 8, 9)
-assert a == 1 and b == 2 and x == 7 and y == 8 and z == 9 and c == [3, 4, 5, 6]
+assert a == 1 and b == 2 and x == 7 and y == 8 and z == 9 and c == [3, 4, 5, 6], (a == 1, b == 2, x == 7, y == 8, z == 9, c == [3, 4, 5, 6])
