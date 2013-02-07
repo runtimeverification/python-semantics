955c501d16b75028ec0c3993ace214303e47f48b
Dwight Guth <dwight.guth@gmail.com>
2013-02-07 14:23:07 -0600

    fix all()

--- programs/testall.py
+++ programs/testall.py
@@ -3,3 +3,10 @@ assert all((1,)) is True
 assert all((0,)) is False
 assert all((1,2,3,4)) is True
 assert all((1,2,3,4,0)) is False
+
+class A:
+  x = 0
+  def __bool__(self): A.x += 1; return False
+
+assert not all((A(), A(), A(), A()))
+assert A.x == 1
