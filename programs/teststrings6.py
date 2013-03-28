c365d1fecd067864b48adda53ab4af24f6b3e00d
Dwight Guth <dwight.guth@gmail.com>
2013-03-28 13:43:34 -0500

    commit final version of str.__getitem__ and str.startswith

--- programs/teststrings6.py
+++ programs/teststrings6.py
@@ -1,15 +1,15 @@
-assert "12345".startswith("1")
-assert "12345".startswith("12")
-assert not "12345".startswith("23")
-assert "12345".startswith("23", 1)
-assert not "12345".startswith("12",1)
-assert "12345".startswith("34", 2, 4)
-assert not "12345".startswith("34", 2, 3)
-assert "12345".startswith(("ab","12",5))
-assert not "12345".startswith(("ab", "34"))
-assert "12345".startswith("5", -1)
-assert "12345".startswith("34", 2, -1)
-assert "12345".startswith("34", 2, 100)
+assert "12345".startswith("1") is True
+assert "12345".startswith("12") is True
+assert "12345".startswith("23") is False
+assert "12345".startswith("23", 1) is True
+assert "12345".startswith("12",1) is False
+assert "12345".startswith("34", 2, 4) is True
+assert "12345".startswith("34", 2, 3) is False
+assert "12345".startswith(("ab","12",5)) is True
+assert "12345".startswith(("ab", "34")) is False
+assert "12345".startswith("5", -1) is True
+assert "12345".startswith("34", 2, -1) is True
+assert "12345".startswith("34", 2, 100) is True
 
 try:
   "12345".startswith((1,))
