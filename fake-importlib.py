6e83400299c80200d962656d9b6fbb6933b7aae1
Dwight Guth <dwight.guth@gmail.com>
2013-04-01 12:15:51 -0500

    fix the fake importlib implementation to be able to successfully perform really basic imports of builtin modules. this allows me to test builtin functions in secondary builtin modules

--- fake-importlib.py
+++ fake-importlib.py
@@ -1,3 +1,8 @@
-def _install(sys, _imp): pass
+def _install(sys_module, _imp_module):
+  global _imp, sys
+  _imp = _imp_module
+  sys = sys_module
 
-def __import__(*x): pass
+def __import__(name, globals=None, locals=None, fromlist=(), level=0):
+  return _imp.init_builtin(name)
+  
